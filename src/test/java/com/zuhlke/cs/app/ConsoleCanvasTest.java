e6740901e5cae53d39e11a575713b8afb2782729
Patrick Amsler <patrick.amsler@zuhlke.com>
2019-02-26 23:34:31 -0800

    CanvasTestFixtures removed. Threshold for performance test added.

--- src/test/java/com/zuhlke/cs/app/ConsoleCanvasTest.java
+++ src/test/java/com/zuhlke/cs/app/ConsoleCanvasTest.java
@@ -5,17 +5,23 @@ import com.zuhlke.cs.model.Rectangle;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-import static com.zuhlke.cs.app.CanvasTestFixtures.*;
 
 public class ConsoleCanvasTest {
 
     @Test
     public void newCanvasCreatesEmptyDrawingAreaWithBorder() {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
+        String expected =
+                "----------------------\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "----------------------\n";
 
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(emptyCanvas20x4(), output);
+        assertEquals(expected, actual);
     }
 
     @Test (expected = IllegalArgumentException.class)
@@ -27,33 +33,54 @@ public class ConsoleCanvasTest {
     public void drawHorizontalLine() {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
         Line line = new Line(1, 2, 6, 2, 'x');
+        String expected =
+                "----------------------\n" +
+                "|                    |\n" +
+                "|xxxxxx              |\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "----------------------\n";
 
         canvas.drawShape(line);
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(canvas20x4WithHorizontalLine(), output);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void drawVerticalLine() {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
         Line line = new Line(2, 1, 2, 4, 'x');
+        String expected =
+                "----------------------\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "----------------------\n";
 
         canvas.drawShape(line);
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(canvas20x4WithVerticalLine(), output);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void drawLineLongerThanCanvas() {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
         Line line = new Line(2, 0, 2, 42, 'x');
+        String expected =
+                "----------------------\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "| x                  |\n" +
+                "----------------------\n";
 
         canvas.drawShape(line);
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(canvas20x4WithVerticalLine(), output);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -61,23 +88,37 @@ public class ConsoleCanvasTest {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
         Line line1 = new Line(1, 5, 4, 5, 'x');
         Line line2 = new Line(42, 1, 42, 3, 'x');
+        String expected =
+                "----------------------\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "|                    |\n" +
+                "----------------------\n";
 
         canvas.drawShape(line1);
         canvas.drawShape(line2);
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(emptyCanvas20x4(), output);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void drawRectangle() {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 4);
-
         Rectangle rectangle = new Rectangle(2,1,4,3);
-        canvas.drawShape(rectangle);
-        String output = canvas.render();
+        String expected =
+                "----------------------\n" +
+                "| xxx                |\n" +
+                "| x x                |\n" +
+                "| xxx                |\n" +
+                "|                    |\n" +
+                "----------------------\n";
 
-        assertEquals(canvas20x4WithRectangle(), output);
+        canvas.drawShape(rectangle);
+        String actual = canvas.render();
+
+        assertEquals(expected, actual);
     }
 
 
@@ -86,13 +127,22 @@ public class ConsoleCanvasTest {
         ConsoleCanvas canvas = new ConsoleCanvas(20, 6);
         Line line1 = new Line(1, 3, 20, 3, 'x');
         Line line2 = new Line(3, 1, 3, 6, 'x');
+        String expected =
+                "----------------------\n" +
+                "|  xyyyyyyyyyyyyyyyyy|\n" +
+                "|  xyyyyyyyyyyyyyyyyy|\n" +
+                "|xxxxxxxxxxxxxxxxxxxx|\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "----------------------\n";
 
         canvas.drawShape(line1);
         canvas.drawShape(line2);
         canvas.fill(5, 1, 'y');
-        String output = canvas.render();
+        String actual = canvas.render();
 
-        assertEquals(canvas20x6WithFilledArea(), output);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -105,23 +155,51 @@ public class ConsoleCanvasTest {
         canvas.drawShape(line2);
 
         // color with y
+        String expected1 =
+                "----------------------\n" +
+                "|  xyyyyyyyyyyyyyyyyy|\n" +
+                "|  xyyyyyyyyyyyyyyyyy|\n" +
+                "|xxxxxxxxxxxxxxxxxxxx|\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "----------------------\n";
         canvas.fill(5, 1, 'y');
-        String output = canvas.render();
-        assertEquals(canvas20x6WithFilledArea(), output);
+        String actual = canvas.render();
+        assertEquals(expected1, actual);
 
         // color with x
+        String expected2 =
+                "----------------------\n" +
+                "|  xxxxxxxxxxxxxxxxxx|\n" +
+                "|  xxxxxxxxxxxxxxxxxx|\n" +
+                "|xxxxxxxxxxxxxxxxxxxx|\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "|  x                 |\n" +
+                "----------------------\n";
         canvas.fill(5, 2, 'x');
-        output = canvas.render();
-        assertEquals(canvas20x6WithFilledArea2(), output);
+        actual = canvas.render();
+        assertEquals(expected2, actual);
 
         // color with z
+        String expected3 =
+                "----------------------\n" +
+                "|  zzzzzzzzzzzzzzzzzz|\n" +
+                "|  zzzzzzzzzzzzzzzzzz|\n" +
+                "|zzzzzzzzzzzzzzzzzzzz|\n" +
+                "|  z                 |\n" +
+                "|  z                 |\n" +
+                "|  z                 |\n" +
+                "----------------------\n";
         canvas.fill(5, 3, 'z');
-        output = canvas.render();
-        assertEquals(canvas20x6WithFilledArea3(), output);
+        actual = canvas.render();
+        assertEquals(expected3, actual);
     }
 
     @Test
     public void fillLargeArea() {
+        int timeThresholdMs = 500;
         int width = 1000;
         int height = 1000;
         ConsoleCanvas canvas = new ConsoleCanvas(width, height);
@@ -129,7 +207,9 @@ public class ConsoleCanvasTest {
         long start = System.currentTimeMillis();
         canvas.fill(5, 1, 'y');
         long stop = System.currentTimeMillis();
-
         System.out.printf("Fill area %sx%s takes: %dms", width, height, stop - start);
+
+        assertTrue("fill large area takes longer than " + timeThresholdMs + " ms",
+                stop - start < timeThresholdMs);
     }
 }
